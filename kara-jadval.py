525448bd0fda3874aeed6fe993282e41cf26655a
Abduvohid_Ahmadjonov <84452468+Abduvohidking@users.noreply.github.com>
2021-06-24 22:15:46 +0300

    Create kara-jadval.py
    karajadval

---
+++ kara-jadval.py
@@ -0,0 +1,12 @@
+# for a in range(11):
+#     for q in range(11):
+#         q=q*2
+#         print(f"{a}x{2}={q}")
+for i in range(1,11):
+    for x in range(1,11):
+        print("{}x{}={}".format(i,x,i*x))
+
+
+
+
+
