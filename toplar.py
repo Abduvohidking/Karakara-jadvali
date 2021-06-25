2a80abf54b34db6c4ee1535efa53b0880e29341f
Abduvohid_Ahmadjonov <84452468+Abduvohidking@users.noreply.github.com>
2021-06-25 23:37:41 +0300

    chalasi bor hali

---
+++ toplar.py
@@ -0,0 +1,74 @@
+# 1 topshiriq
+
+# a = int(input("a="))
+# for x in range(1,a+1,1):
+#     x=x**2
+#     print(x)
+
+# 2 topshiriq
+
+# a = int(input("a="))
+# b = int(input("b="))
+# if a>b:
+#     for x in range(b,a,1):
+#         print(x)
+# else:
+#     for y in range(a,b,1):
+#         print(y)
+
+# 3 topshiriq
+
+# a = int(input("a="))
+# b = 1
+# for x in range(1,a):
+#     b= b*(x+1)
+# print(b)
+
+# 4 topshiriq
+
+# n = int(input('Son kiriting: '))
+# s = 0
+# for son in range(0, n + 1, 2): 
+#     s += son
+# print(s)
+
+# 5 topshiriq
+
+# n = int(input('Son kiriting: '))
+# s = 0
+# for son in range(0, n + 1, 1): 
+#       s += son
+#       print
+# print(s/n+1)
+
+
+
+# 6 topshiriq
+
+# a = int(input("a="))
+# b = 1
+# for x in range(1,a):
+#     b= b*(x+1)
+# print(b**0.5)
+
+
+# 7 topshiriq
+
+# n = int(input('Son kiriting: '))
+# s = 0
+# for son in range(1, n + 1, 2): 
+#     s += 1/son
+# print(s)
+
+# 8 topshiriq
+
+# yo`l topa olmadim
+
+# 9 topshiriq
+# a = input("Oy nomini kiriting")
+# for x in a:
+#     for y in range(1,31):
+#         print(f"{x}  {y}")
+# +998973059025 Abdullayev Umid 
+
+
